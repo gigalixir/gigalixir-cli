a256ac7b7bc78041bc70e052faee13b9dddce600
Jesse Shieh <jesse@gigalixir.com>
2017-05-03 14:41:16 -0700

    hot upgrade e2e test works now.

--- e2e/test.py
+++ e2e/test.py
@@ -38,16 +38,16 @@ def test_everything():
                 if r.status_code != 200:
                     # wait 5 seconds
                     logging.info('Received %s' % r.status_code)
-                    logging.info('Waiting 30 seconds to try again.')
-                    time.sleep(30)
+                    logging.info('Waiting 5 seconds to try again.')
+                    time.sleep(5)
                 else:
                     logging.info('Pass.')
                     break
             except requests.exceptions.ConnectionError as e:
                 # wait 5 seconds
                 logging.info('ConnectionError: %s' % e)
-                logging.info('Waiting 30 seconds to try again.')
-                time.sleep(30)
+                logging.info('Waiting 5 seconds to try again.')
+                time.sleep(5)
         else:
             logging.info('Exhausted retries. Be sure to scale down your app manually and other necessary cleanup since we are aborting now.')
             assert False
@@ -81,6 +81,41 @@ def test_everything():
         assert result.exit_code == 0
         configs = json.loads(result.output)
         assert configs == {}
+
+        # hot upgrade
+        with cd("gigalixir-getting-started"):
+            gigalixir.shell.cast("""git -c http.extraheader="GIGALIXIR-HOT:true" push gigalixir origin/v0.0.2:master""")
+        logging.info('Completed Hot Upgrade.')
+        start_time = timeit.default_timer()
+        url = 'https://%s.gigalixirapp.com/' % app_name
+        for i in range(30):
+            try:
+                logging.info('Attempt: %s/30: Checking %s' % (i, url))
+                r = requests.get(url)
+                if r.status_code != 200:
+                    # wait 5 seconds
+                    logging.info('Received %s' % r.status_code)
+                    logging.info('Waiting 5 seconds to try again.')
+                    time.sleep(5)
+                else:
+                    if "0.0.2" not in r.text:
+                        # wait 5 seconds
+                        logging.info('0.0.2 not found.')
+                        logging.info('Waiting 5 seconds to try again.')
+                        time.sleep(5)
+                    else:
+                        logging.info('Pass.')
+                        break
+            except requests.exceptions.ConnectionError as e:
+                # wait 5 seconds
+                logging.info('ConnectionError: %s' % e)
+                logging.info('Waiting 5 seconds to try again.')
+                time.sleep(5)
+        else:
+            logging.info('Exhausted retries. Be sure to scale down your app manually and other necessary cleanup since we are aborting now.')
+            assert False
+        elapsed = timeit.default_timer() - start_time
+        logging.info("Elapsed time: %s" % elapsed)
 
         # scale down to 0
         result = runner.invoke(gigalixir.cli, ['scale', app_name, '--replicas=0'])
