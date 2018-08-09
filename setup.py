3580a2a7bff8f0aa6fc19be43792dd0671b463a9
Jesse <jesse@gigalixir.com>
2018-08-08 17:27:00 -0700

    bump version to 1.0.0-beta.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     url='https://github.com/gigalixir/gigalixir-cli',
     author='Jesse Shieh',
     author_email='jesse@gigalixir.com',
-    version='0.27.0',
+    version='1.0.0-beta.0',
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
