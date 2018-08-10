e782f33e5e36b9abbf6a2aa1a65cdd7990f452ec
Jesse <jesse@gigalixir.com>
2018-08-09 21:05:03 -0700

    fix for python3.

--- gigalixir/presenter.py
+++ gigalixir/presenter.py
@@ -2,7 +2,10 @@ import click
 import json
 from pygments import highlight, lexers, formatters
 
+# Fix Python 2.x.
+from six import u as unicode
+
 def echo_json(data):
     formatted_json = json.dumps(data, indent=2, sort_keys=True)
-    colorful_json = highlight(unicode(formatted_json, 'UTF-8'), lexers.JsonLexer(), formatters.TerminalFormatter())
+    colorful_json = highlight(unicode(formatted_json), lexers.JsonLexer(), formatters.TerminalFormatter())
     click.echo(colorful_json)
