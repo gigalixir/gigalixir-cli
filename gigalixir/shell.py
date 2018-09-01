65553da397c2ccdf18fdec09a5630d2d295e4c28
Jesse <jesse@gigalixir.com>
2018-09-01 10:46:18 -0700

    on shell cmd error, still print out stdout to see the error.

--- gigalixir/shell.py
+++ gigalixir/shell.py
@@ -7,7 +7,14 @@ def call(cmd):
     # object which needs to be converted into a string.
     # for python2, it returns a string and calling decode() on it
     # gives you a unicode which is fine.
-    return subprocess.check_output(cmd.split()).decode().strip()
+    pipes = subprocess.Popen(cmd.split(), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    std_out, std_err = pipes.communicate()
+    if pipes.returncode != 0:
+        # an error happened!
+        # err_msg = "%s. Code: %s" % (std_err.strip(), pipes.returncode)
+        raise Exception(std_out)
+    return std_out.decode().strip()
+
 
 # if you only care that the cmd succeeded, call this
 def cast(cmd):
