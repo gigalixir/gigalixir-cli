d6b49d08161edf6c246cd7c8b00bafc5fcf9e159
Jesse <jesse@gigalixir.com>
2018-10-07 17:02:26 -0700

    use utf-8 encoding for all shell commands. unsure if that is correct or not. see https://stackoverflow.com/questions/52643719/what-encoding-to-use-for-the-output-of-subprocess-check-outputgit-remote-v

--- gigalixir/shell.py
+++ gigalixir/shell.py
@@ -2,18 +2,13 @@ import subprocess
 
 # if you care about the cmd output use call
 def call(cmd):
-    # kind of crappy, but we call decode() here for python3 
-    # compatibility. in python3 check_output returns a bytes-like
-    # object which needs to be converted into a string.
-    # for python2, it returns a string and calling decode() on it
-    # gives you a unicode which is fine.
     pipes = subprocess.Popen(cmd.split(), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     std_out, std_err = pipes.communicate()
     if pipes.returncode != 0:
         # an error happened!
         # err_msg = "%s. Code: %s" % (std_err.strip(), pipes.returncode)
         raise GigalixirShellError(pipes.returncode, cmd, output=std_out, stderr=std_err)
-    return std_out.decode().strip()
+    return std_out.decode("utf-8").strip()
 
 
 # if you only care that the cmd succeeded, call this
