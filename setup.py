5346c5fc656c5e69316f19d36bf8ea94eb1433a0
Jesse <jesse@gigalixir.com>
2018-11-19 10:02:16 -0800

    1.0.14

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     url='https://github.com/gigalixir/gigalixir-cli',
     author='Jesse Shieh',
     author_email='jesse@gigalixir.com',
-    version='1.0.13',
+    version='1.0.14',
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
