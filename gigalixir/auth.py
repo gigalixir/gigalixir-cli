a98d99da13e4121ad378b52689e9c321d734147e
Jesse <jesse@gigalixir.com>
2019-07-08 10:38:15 -0700

    better auth error message.

--- gigalixir/auth.py
+++ gigalixir/auth.py
@@ -1,5 +1,5 @@
 class AuthException(Exception):
     def __init__(self):
         # Call the base class constructor with the parameters it needs
-        message = "Sorry, you do not have access. Try running gigalixir login or checking your ~/.netrc file."
+        message = "Sorry, you do not have access to that app. Try passing the app name explicitly with the `-a` flag. If that doesn't work, try running `gigalixir login` or check your ~/.netrc file."
         super(AuthException, self).__init__(message)
