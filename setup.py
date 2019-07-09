ae2aa45e2c8eab081c16ab3aa47f11748454bfe3
Jesse <jesse@gigalixir.com>
2019-07-08 18:56:30 -0700

    v1.0.20

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     url='https://github.com/gigalixir/gigalixir-cli',
     author='Jesse Shieh',
     author_email='jesse@gigalixir.com',
-    version='1.0.19',
+    version='1.0.20',
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
