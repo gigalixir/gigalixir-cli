44b4dd30aaf2fb09f9b9834ab9ee65cdf9f8c780
Jesse <jesse@gigalixir.com>
2021-11-12 19:24:13 -0500

    v1.2.4

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     url='https://github.com/gigalixir/gigalixir-cli',
     author='Jesse Shieh',
     author_email='jesse@gigalixir.com',
-    version='1.2.3',
+    version='1.2.4',
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
