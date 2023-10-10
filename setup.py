65439af081bc83381b31d13bb18ef7be85919f71
Tim <tim@gigalixir.com>
2023-10-10 10:35:39 -0500

    chore: update rollbar token

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     url='https://github.com/gigalixir/gigalixir-cli',
     author='Tim Day',
     author_email='tim@gigalixir.com',
-    version='1.8.1',
+    version='1.8.2',
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
